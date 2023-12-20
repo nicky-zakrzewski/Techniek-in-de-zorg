c535ba4cfd70ae037486a58e7f96d658cef45f5f
calvinliaci <liacicalvin@gmail.com>
2023-12-20 23:37:50 +0100

    added sprite

---
+++ TIDZ/Characters/player_nurse.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 200
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+func _ready():
+	pass
+	update_animation_parameters(starting_direction)
+
+func _physics_process(_delta):
+	var input_direction = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	input_direction = input_direction.normalized()
+	if input_direction == Vector2.ZERO:
+		apply_friction(1200 * _delta)
+	else:
+		velocity = input_direction * move_speed
+	update_animation_parameters(input_direction)
+	move_and_slide()
+	print(velocity)
+	pick_new_state()
+
+func update_animation_parameters(move_input : Vector2):
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
+
+func apply_friction(amount):
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
